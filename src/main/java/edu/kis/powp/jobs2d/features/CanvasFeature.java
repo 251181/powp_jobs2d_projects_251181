6da9adeeaffc778f106daa7f878a4d94c45a55c1
Albert G. <252793@edu.p.lodz.pl>
2026-04-17 16:14:28 +0200

    Cleaned up all the unused imports.

--- src/main/java/edu/kis/powp/jobs2d/features/CanvasFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/CanvasFeature.java
@@ -1,20 +1,11 @@
 package edu.kis.powp.jobs2d.features;
 
 import edu.kis.legacy.drawer.shape.LineFactory;
-import edu.kis.legacy.drawer.shape.ILine;
-import edu.kis.legacy.drawer.shape.line.AbstractLine;
-import edu.kis.legacy.drawer.shape.line.SpecialLine;
 import edu.kis.powp.appbase.Application;
-import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.LoggerDriver;
 import edu.kis.powp.jobs2d.canvas.CanvasFormat;
 import edu.kis.powp.jobs2d.canvas.PaperFormat;
 import edu.kis.powp.jobs2d.canvas.TriangleFormat;
-import edu.kis.powp.jobs2d.command.CompoundCommand;
-import edu.kis.powp.jobs2d.command.SimpleComplexCommandBuilder;
 import edu.kis.powp.jobs2d.drivers.adapter.LineDriverAdapter;
-
-import java.awt.geom.PathIterator;
 
 public class CanvasFeature implements IFeature {
     private static CanvasFormat currentFormat;
